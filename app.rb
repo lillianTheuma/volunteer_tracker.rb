827fdfa61b16d47b3c1d1929214e982d6ddf424c
Student <example@example.com>
2020-01-10 10:20:39 -0800

    Added some HTTP request responses

--- app.rb
+++ app.rb
@@ -4,3 +4,36 @@ require('./lib/volunteer')
 require('./lib/project')
 require('pry')
 require("pg")
+
+# /
+get('/') do
+  erb(:main)
+end
+#projects
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+post('/projects') do
+  title = params[:title]
+  id = params[:id]
+  project = Project.new({:title => title, :id => id})
+  project.save()
+  redirect to ('/projects')
+end
+
+get('/projects/new') do
+  erb(:new_project)
+end
+
+get('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
+
+patch('/projects/:id') do
+  project = Project.find(params[:id].to_i())
+  project.update(:title => params[:title])
+end
